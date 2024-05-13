0c4079fb49f97ba41253f1dd5164f9487cc9ff1d
youhei-0009 <yauchi.youhei0009@gmail.com>
2024-05-13 18:19:12 +0900

    temtative commit

--- react-test1/main.js
+++ react-test1/main.js
@@ -1,1 +1,17 @@
 import './style.css'
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  console.log("div");
+
+}
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
